bf6db2a1dedbc44a3af30f362b582d6f54374b5e
lucianapda <lpa@furb.br>
2025-02-24 11:06:54 -0300

    Início do exercício 2

---
+++ Unidade2/src/Exe02.java
@@ -0,0 +1,17 @@
+public class Exe02 {
+
+    //Leia 2 valores inteiros e 
+    //imprima sua multiplicação.
+
+    //Entrada: valor1, valor2
+    //Saída: multiplicacao
+    //Processo: multiplicacao = valor1 * valor2
+    //Testes:
+    //valor1        |2|10|-5
+    //valor2        |4|10|10
+    //multiplicacao |8|100|-50
+
+    public static void main(String[] args) {
+        //int valor1 = teclado.nextInt();
+    }
+}
